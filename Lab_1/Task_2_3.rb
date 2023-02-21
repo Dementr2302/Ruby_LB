b2fe70bbde6e4a539265a0d8af21fc520ed72103
Dementr2302 <94387603+Dementr2302@users.noreply.github.com>
2023-02-21 10:24:06 +0300

    .

---
+++ Lab_1/Task_2_3.rb
@@ -0,0 +1,18 @@
+def sum_ch(ch)
+  summ=0
+  while ch>0
+    summ+=ch%10
+    ch/=10
+  end
+  summ
+end
+
+def pr_ch(ch)
+  p=1
+  1.upto(ch) do |i|
+    if  ch%i==0 && sum_ch(i)<sum_ch(ch)
+      p*=i
+    end
+  end
+  p
+end
