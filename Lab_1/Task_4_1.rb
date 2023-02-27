b272e10dce6629e38885f0523bbe62ca2df01d80
Dementr2302 <94387603+Dementr2302@users.noreply.github.com>
2023-02-27 16:00:36 +0300

    Create Task_4_1.rb

---
+++ Lab_1/Task_4_1.rb
@@ -0,0 +1,19 @@
+def count_elements(array)
+  max = array[0]
+  max_index = 0
+  index = 0
+
+  array.each do |num|
+    if num >= max
+      max = num
+      max_index = index
+    end
+    index += 1
+  end
+
+  if max_index == array.length - 1
+    return 0
+  else
+    return array.length - 1 - max_index
+  end
+end
