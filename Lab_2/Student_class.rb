112c576db807719f39524d9a8c48cb7d353dcf41
Dementr <dementr@mail.com>
2023-03-12 17:42:10 +0300

    чтоб красиво было

--- Lab_2/Student_class.rb
+++ Lab_2/Student_class.rb
@@ -1,6 +1,6 @@
 class Student
   attr_accessor :id, :last_name, :first_name, :father_name, :phone, :telegram, :email, :github
-  def initialize(id, last_name, first_name, father_name = nil, phone = nil, telegram = nil, email = nil, github = nil)
+  def initialize(id, last_name, first_name, father_name: nil, phone: nil, telegram: nil, email: nil, github: nil)
     @id = id
     @last_name = last_name
     @first_name = first_name
@@ -12,10 +12,18 @@ class Student
   end
   def get_info
     puts "Студент #{self.last_name} #{self.first_name} #{self.father_name}"
-    puts "Telegram: #{self.telegram}"
-    puts "Телефон: #{self.phone}"
-    puts "Почта: #{self.email}"
-    puts "Github: #{self.github}"
+      if self.telegram != nil
+        puts "Telegram: #{self.telegram}"
+      end
+      if self.phone != nil
+        puts "Телефон: #{self.phone}"
+      end
+      if self.email != nil
+        puts "Почта: #{self.email}"
+      end
+      if self.github != nil
+        puts "Github: #{self.github}"
+      end
     end
 
 end
--- Lab_2/main.rb
+++ Lab_2/main.rb
@@ -1,7 +1,7 @@
 require_relative 'Student_class.rb'
 
-student1 = Student.new(1, "Байрамов", "Дмитрий", "Дмитиревич", "+123456789", "@dementr", "dmitr@mail.com", "dementr2302")
-student2 = Student.new(1, "Байрамов", "Дмитрий", "Дмитиревич", "+123456789", "@dementr", "dmitr@mail.com", "dementr2302")
+student1 = Student.new(1, "Байрамов", "Дмитрий", father_name: "Дмитиревич", phone: "+123456789", telegram: "@dementr", email: "dmitr@mail.com", github: "dementr2302")
+student2 = Student.new(2, "Байрамов", "Дмитрий", father_name: "Дмитиревич", phone: "+123456789", telegram: "@dementr", email: "dmitr@mail.com", github: "dementr2302")
 
 puts student1.get_info
 puts student2.get_info
