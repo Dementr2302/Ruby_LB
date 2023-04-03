64426282ff890e1981fa407c7faff71478a21705
Dementr <dementr@mail.com>
2023-04-03 18:19:03 +0300

    short_student

--- Lab_2/main.rb
+++ Lab_2/main.rb
@@ -1,13 +1,14 @@
 require_relative 'Student.rb'
+require_relative 'student_short'
+#
+# student1 = Student.new('Иванов', 'Иван', 'Иванович', { id: 1, phone: '+7 (999) 123-45-67', telegram: '@ivanov', email: 'ivanov@example.com', github: 'https://github.com/ivanov' })
+# puts student1.to_s
+#
+#
+# begin
+#   student4 = Student.new('Петров', 'Петр', 'Петрович', { phone: '+7 (999) 123-45-67',github: 'github.com/petrov' })
+#   student4.phone_err = 'githutrov' # установка значения github с помощью нового метода
+# rescue PhoneError => e
+#   puts e.message
+# end
 
-
-student1 = Student.new('Иванов', 'Иван', 'Иванович', { id: 1, phone: '+7 (999) 123-45-67', telegram: '@ivanov', email: 'ivanov@example.com', github: 'https://github.com/ivanov' })
-puts student1.to_s
-
-
-begin
-  student4 = Student.new('Петров', 'Петр', 'Петрович', { phone: '+7 (999) 123-45-67',github: 'github.com/petrov' })
-  student4.phone_err = 'githutrov' # установка значения github с помощью нового метода
-rescue PhoneError => e
-  puts e.message
-end
