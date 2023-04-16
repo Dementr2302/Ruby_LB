806a3e6fdd3a3059372a27c3f548e6fb2dd5ca16
Dementr <dementr@mail.com>
2023-04-16 19:02:06 +0300

    2.4.3

--- Lab_2/test.rb
+++ Lab_2/test.rb
@@ -1,10 +1,11 @@
 require_relative 'student_list_txt'
 require_relative 'student_list_json'
+require_relative 'student_list_yaml'
 
 
-a = StudentListTxt.new()
-a.read_file("/Users/dmitrijbajramov/Desktop/Lab_2/Ruby_LB/Lab_2/student_list.txt")
-puts a.students
+# a = StudentListTxt.new()
+# a.read_file("/Users/dmitrijbajramov/Desktop/Lab_2/Ruby_LB/Lab_2/student_list.txt")
+# puts a.students
 
 # puts a.get_k_n_student_short_list(1,2)
 # a.write_to_file("/Users/dmitrijbajramov/Desktop/Lab_2/Ruby_LB/Lab_2/student_list.txt")
@@ -12,8 +13,12 @@ puts a.students
 
 list=[1,2,3]
 
-a = StudentListJson.new()
-a.read_file("/Users/dmitrijbajramov/Desktop/Lab_2/Ruby_LB/Lab_2/student_list.json")
+# a = StudentListJson.new()
+# a.read_file("/Users/dmitrijbajramov/Desktop/Lab_2/Ruby_LB/Lab_2/student_list.json")
+
+a = StudentListYaml.new()
+a.read_file("/Users/dmitrijbajramov/Desktop/Lab_2/Ruby_LB/Lab_2/student_list.yaml")
 # puts a.students
-a.sorted_by_name
-a.write_to_file("/Users/dmitrijbajramov/Desktop/Lab_2/Ruby_LB/Lab_2/student_list_out.json")
+# a.sorted_by_name
+# a.write_to_file("/Users/dmitrijbajramov/Desktop/Lab_2/Ruby_LB/Lab_2/student_list_out.json")
+a.write_to_file("/Users/dmitrijbajramov/Desktop/Lab_2/Ruby_LB/Lab_2/student_list_out.yaml")
